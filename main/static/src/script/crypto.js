2354c02d0269d982a91d18efc7a83cb04d1334db
Panayiotis Lipiridis <lipiridis@gmail.com>
2018-01-17 13:33:08 +0100

    Fix

--- main/static/src/script/crypto.js
+++ main/static/src/script/crypto.js
@@ -1,36 +1,41 @@
 window.fetchSpentPrice = function() {
   clear_notifications();
   $('#fetch').text('fetching...');
-  var regExp = /\(([^)]+)\)/;
-  var acquired = $('#acquired_amount').val();
-  var spentMatches = regExp.exec(
+  const regExp = /\(([^)]+)\)/;
+  const acquired = $('#acquired_amount').val();
+  const spentMatches = regExp.exec(
     $('#spent_currency_key')
       .find(`option[value='${$('#spent_currency_key').val()}']`)
       .text(),
   );
   spentCode = spentMatches[1];
-  var acquiredMatches = regExp.exec(
+  const acquiredMatches = regExp.exec(
     $('#acquired_currency_key')
       .find(`option[value='${$('#acquired_currency_key').val()}']`)
       .text(),
   );
   acquiredCode = acquiredMatches[1];
-  var ts = new Date($('#date').val()).getTime() / 1000;
+  const MSEC_IN_SEC = 1000;
+  const ts = new Date($('#date').val()).getTime() / MSEC_IN_SEC;
 
-  fetch(`/api/v1/crypto/price/historical/?tsym=${spentCode}&fsym=${acquiredCode}&ts=${ts}`)
+  fetch(
+    `/api/v1/crypto/price/historical/?tsym=${spentCode}&fsym=${acquiredCode}&ts=${ts}`,
+  )
     .then(resp => resp.json())
-    .then(function(data) {
-      var rate = data[`${acquiredCode}${spentCode}`];
+    .then(data => {
+      const rate = data[`${acquiredCode}${spentCode}`];
       $('#spent_amount').val(acquired * rate);
       $('#fetch').text('fetch');
-      show_notification(`Spent amount updated with a ${acquiredCode}/${spentCode} : ${rate}${spentCode}`, 'success');
+      show_notification(
+        `Spent amount updated with a ${acquiredCode}/${spentCode} : ${rate}${spentCode}`,
+        'success',
+      );
     })
-    .catch(function(error) {
+    .catch(error => {
       $('#fetch').text('fetch');
       show_notification(`Something went wrong. Please try again.`, 'danger');
     });
 };
-
 
 $('#fetch').click(event => {
   event.preventDefault();
